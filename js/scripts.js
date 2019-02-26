cfbe1c09429afc23d8255f382f1b61a837454621
pwachira900@gmail.com <pwachira900@gmail.com>
2019-02-26 10:28:33 +0300

    fixed indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $("#resethtml").click(function reset() {
 
 // Un hide buttons  Dojo options
 $(document).ready(function() {
-  $("button#js-nav").click(function javascriptQuiz(){
+  $("button#js-nav").click(function javascriptQuiz() {
     $("#css-hidden").hide();
     $("#html-hidden").hide();
     $("#js-hidden").slideToggle();
@@ -37,14 +37,14 @@ $(document).ready(function() {
 
 });
 $(document).ready(function() {
-  $("button#html-nav").click(function cssQuiz(){
+  $("button#html-nav").click(function cssQuiz() {
     $("#css-hidden").hide();
     $("#js-hidden").hide();
     $("#html-hidden").slideToggle();
   });
 });
 $(document).ready(function() {
-  $("button#css-nav").click(function htmlQuiz(){
+  $("button#css-nav").click(function htmlQuiz() {
     $("#js-hidden").hide();
     $("#html-hidden").hide();
     $("#css-hidden").fadeIn();
@@ -53,46 +53,46 @@ $(document).ready(function() {
 
 //
 // // IDEA:  calculating marks for each question  using a loop that loops through all the radio buttons per question
- $(document).ready(function(){
-   $("button#jsSubmit").click(function jsmarks(event){
-     var score = 0;
-     $("input:radio[name=js1]:checked,input:radio[name=js2]:checked,input:radio[name=js3]:checked,input:radio[name=js4]:checked,input:radio[name=js5]:checked").each(function() {
-       count = score += parseInt($(this).val());
-       percentage = (count/50)*100;
-     });
+$(document).ready(function() {
+  $("button#jsSubmit").click(function jsmarks(event) {
+    var score = 0;
+    $("input:radio[name=js1]:checked,input:radio[name=js2]:checked,input:radio[name=js3]:checked,input:radio[name=js4]:checked,input:radio[name=js5]:checked").each(function() {
+      count = score += parseInt($(this).val());
+      percentage = (count / 50) * 100;
+    });
     document.getElementById("js-results").innerHTML = " your percentage score was " + percentage + "%";
   });
- });
+});
 
- $(document).ready(function(){
-   $("button#cssSubmit").click(function cssmarks(event){
-     var score = 0;
-     $("input:radio[name=css1]:checked,input:radio[name=css2]:checked,input:radio[name=css3]:checked,input:radio[name=css4]:checked,input:radio[name=css5]:checked").each(function() {
-     // score += parseInt($(this).val());
-     count = score += parseInt($(this).val());
-     percentage = (count/50)*100;
-     });
-      document.getElementById("css-results").innerHTML = " your percentage score was " + percentage + "%";
+$(document).ready(function() {
+  $("button#cssSubmit").click(function cssmarks(event) {
+    var score = 0;
+    $("input:radio[name=css1]:checked,input:radio[name=css2]:checked,input:radio[name=css3]:checked,input:radio[name=css4]:checked,input:radio[name=css5]:checked").each(function() {
+      // score += parseInt($(this).val());
+      count = score += parseInt($(this).val());
+      percentage = (count / 50) * 100;
+    });
+    document.getElementById("css-results").innerHTML = " your percentage score was " + percentage + "%";
   });
- });
+});
 
- $(document).ready(function(){
-   $("button#htmlSubmit").click(function htmlmarks(event){
-     var score = 0;
-     $("input:radio[name=html1]:checked,input:radio[name=html2]:checked,input:radio[name=html3]:checked,input:radio[name=html4]:checked,input:radio[name=html5]:checked").each(function() {
-       count = score += parseInt($(this).val());
-       percentage = (count/50)*100;
-     });
+$(document).ready(function() {
+  $("button#htmlSubmit").click(function htmlmarks(event) {
+    var score = 0;
+    $("input:radio[name=html1]:checked,input:radio[name=html2]:checked,input:radio[name=html3]:checked,input:radio[name=html4]:checked,input:radio[name=html5]:checked").each(function() {
+      count = score += parseInt($(this).val());
+      percentage = (count / 50) * 100;
+    });
     document.getElementById("html-results").innerHTML = " your percentage score was " + percentage + "%";
   });
- });
+});
 //diplay results
- // scrolldown progress bar
- $(window).scroll(function() {
-   var s = $(window).scrollTop(),
-     d = $(document).height(),
-     c = $(window).height();
-   scrollPercent = (s / (d - c)) * 100;
-   var position = scrollPercent;
-   $("#progressbar").attr('value', position);
- });
+// scrolldown progress bar
+$(window).scroll(function() {
+  var s = $(window).scrollTop(),
+    d = $(document).height(),
+    c = $(window).height();
+  scrollPercent = (s / (d - c)) * 100;
+  var position = scrollPercent;
+  $("#progressbar").attr('value', position);
+});
